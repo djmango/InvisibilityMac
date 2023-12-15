3f6b436d1cdb26770af3c4ef4eef83debbfdd99f
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2023-12-15 03:28:30 -0500

    :recycle: (Project) Name to Piedpiper from PiedPiper

--- Piedpiper/Views/AppView.swift
+++ Piedpiper/Views/AppView.swift
@@ -1,13 +1,6 @@
 import SwiftUI
 import ViewState
 
-struct YourView_Previews: PreviewProvider {
-    static var previews: some View {
-        AppView()
-    }
-}
-
-
 struct AppView: View {
     @Environment(CommandViewModel.self) private var commandViewModel
     
