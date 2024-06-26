f66e388142b5352dbda7d24772269a1d89b99b68
Rrocketter <rahul.rocket711@gmail.com>
2024-06-26 12:58:34 -0400

    loginCard

---
+++ Invisibility/Views/SettingsViews/LoginCardView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginCardView.swift
+//  Invisibility
+//
+//  Created by Rahul Gupta on 6/25/24.
+//  Copyright © 2024 Invisibility Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginCardView()
+}
