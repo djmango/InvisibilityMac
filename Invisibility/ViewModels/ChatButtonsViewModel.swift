d476936217cbf5ef8ce42f03eecbe8c7dd3ab92e
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-06-26 15:31:20 -0700

    :recycle: (Buttons) Refactor chat buttons to private local viewmodel, snappier!

---
+++ Invisibility/ViewModels/ChatButtonsViewModel.swift
@@ -0,0 +1,68 @@
+import Combine
+import Foundation
+
+class ChatButtonsViewModel: ObservableObject {
+    @Published private(set) var isGenerating: Bool = false
+    @Published private(set) var isRecording: Bool = false
+    @Published private(set) var whoIsVisible: mainWindowView = .chat
+
+    private var cancellables = Set<AnyCancellable>()
+
+    private let chatViewModel: ChatViewModel = .shared
+    private let mainWindowViewModel: MainWindowViewModel = .shared
+    private let messageViewModel: MessageViewModel = .shared
+    private let screenRecorder: ScreenRecorder = .shared
+    private let screenshotManager: ScreenshotManager = .shared
+    private let windowManager: WindowManager = .shared
+
+    var isShowingHistory: Bool {
+        whoIsVisible == .history
+    }
+
+    init() {
+        Task { @MainActor in
+            await screenRecorder.$isRunning
+                .receive(on: DispatchQueue.main)
+                .assign(to: \.isRecording, on: self)
+                .store(in: &cancellables)
+        }
+
+        mainWindowViewModel.$whoIsVisible
+            .sink { [weak self] whoIsVisible in
+                guard let self else { return }
+                self.whoIsVisible = whoIsVisible
+            }
+            .store(in: &cancellables)
+
+        messageViewModel.$isGenerating
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.isGenerating, on: self)
+            .store(in: &cancellables)
+    }
+
+    @MainActor func toggleRecording() {
+        screenRecorder.toggleRecording()
+    }
+
+    @MainActor func newChat() {
+        _ = chatViewModel.newChat()
+    }
+
+    func captureScreenshot() {
+        Task {
+            await screenshotManager.capture()
+        }
+    }
+
+    @MainActor func changeView(to view: mainWindowView) {
+        _ = mainWindowViewModel.changeView(to: view)
+    }
+
+    @MainActor func switchSide() {
+        windowManager.switchSide()
+    }
+
+    @MainActor func stopGenerating() {
+        messageViewModel.stopGenerating()
+    }
+}
